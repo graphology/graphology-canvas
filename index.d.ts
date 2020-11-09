4bd93ac67de622f8780a77996e416254a06f6363
Yomguithereal <guillaumeplique@gmail.com>
2020-11-09 18:02:14 +0100

    Upgrading deps

---
+++ index.d.ts
@@ -0,0 +1,4 @@
+import Graph from 'graphology-types';
+
+export default function render(graph: Graph, outputPath: string, callback: () => void): void;
+export default function render(graph: Graph, outputPath: string, settings: any, callback: () => void): void;
